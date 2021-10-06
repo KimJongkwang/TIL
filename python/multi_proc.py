21fce4f629540237b1414fbf4fc3d0efa8b5378b
KimJongKwang <kjk6646@naver.com>
2021-10-07 00:44:54 +0900

    211007-python

--- python/multi_proc.py
+++ python/multi_proc.py
@@ -1,6 +1,8 @@
+import multiprocessing
+
 def return_multiply(n):
     def cal(m):
         return m ** 2
-    import multiprocessing    
     pool = multiprocessing.Pool(2)
-    return pool.map(cal, n)
+    return pool.apply(cal(n))
+
