8ed878e612f2c37bfb46616c96ab786862f1fdc6
KimJongKwang <kjk6646@naver.com>
2021-10-14 23:42:44 +0900

    211014-python

--- python/multi_proc.py
+++ python/multi_proc.py
@@ -1,8 +1,25 @@
 import multiprocessing
+import time
+import os
 
 def return_multiply(n):
-    def cal(m):
-        return m ** 2
-    pool = multiprocessing.Pool(2)
-    return pool.apply(cal(n))
+    
+    print(f"{n}의 PID는 :",os.getpid(),"입니다.")
+    time.sleep(0.1)
+    return n ** 2
 
+def single_processing():
+    start = time.time()
+    print(list(map(return_multiply, range(1, 20))))
+    proc_time = time.time() - start
+    print(round(proc_time, 1), "초")
+
+def multi_processing():
+    start = time.time()
+    pool = multiprocessing.Pool(4)
+    print(pool.map(return_multiply, range(1, 20)))
+    proc_time = time.time() - start
+    print(round(proc_time, 1), "초")
+
+if __name__=="__main__":
+    multi_processing()
